3f4630af414223d6c1bdedad9a55630097b65ca5
galtso <galcho_ta@yahoo.com>
2022-05-11 06:22:14 +0300

    Added BasicStackOperations.java

---
+++ Stacks And Queues/BasicStackOperations.java
@@ -0,0 +1,30 @@
+package StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BasicStackOperations {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int s = scan.nextInt();
+        int x = scan.nextInt();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            stack.push(scan.nextInt());
+        }
+        for (int i = 0; i < s; i++) {
+            stack.pop();
+        }
+        if (!stack.isEmpty()) {
+            if (stack.contains(x)) {
+                System.out.println("true");
+            } else {
+                System.out.println(Collections.min(stack));
+            }
+        } else {
+            System.out.println(0);
+        }
+    }
+}
